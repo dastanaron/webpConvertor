a3512d86bd514afb053e01124b15114a59177ec0
dastanaron <flow199@yandex.ru>
2021-05-26 23:05:16 +0300

    Added convertor

--- src/main.go
+++ src/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"log"
+	"net/http"
+
+	"github.com/nickalie/go-webpbin"
 )
 
 func main() {
-	startTime := time.Now()
+	http.HandleFunc("/", convert)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-	endTime := time.Now()
+func convert(response http.ResponseWriter, request *http.Request) {
+	//response.Header().Set("content-type", "image/webp")
+	err := webpbin.NewCWebP().
+		Quality(80).
+		InputFile("../data/test.jpg").
+		Output(response).
+		Run()
+	CheckError(err)
+}
 
-	fmt.Println("Time:", endTime.Sub(startTime))
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
