e54053b1726b85e52e55f7ac3b81f68a1c4f2509
dastanaron <flow199@yandex.ru>
2021-05-28 18:52:08 +0300

    Added downloading image from source

--- src/main.go
+++ src/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
 	"log"
 	"net/http"
 
@@ -8,22 +12,88 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", convert)
+	http.HandleFunc("/", handleRequest)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func convert(response http.ResponseWriter, request *http.Request) {
-	//response.Header().Set("content-type", "image/webp")
+func handleRequest(response http.ResponseWriter, request *http.Request) {
+	downloadedImage, err := downloadImage(request)
+	if err != nil {
+		BuildErrorResponse(err, response, 422)
+		return
+	}
+	convert(response, request, downloadedImage)
+}
+
+func convert(response http.ResponseWriter, request *http.Request, image *bytes.Buffer) {
 	err := webpbin.NewCWebP().
 		Quality(80).
-		InputFile("../data/test.jpg").
+		Input(image).
 		Output(response).
 		Run()
 	CheckError(err)
+}
+
+func downloadImage(request *http.Request) (*bytes.Buffer, error) {
+	params := request.URL.Query()
+
+	srcParam, ok := params["src"]
+
+	if !ok || len(srcParam) < 1 {
+		return nil, errors.New("Not found source of image")
+	}
+
+	imageUrl := srcParam[0]
+
+	response, err := http.Get(imageUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return nil, errors.New("Received non 200 response code")
+	}
+
+	var downloadedFile bytes.Buffer
+
+	_, err = io.Copy(&downloadedFile, response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &downloadedFile, nil
 }
 
 func CheckError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func BuildErrorResponse(err error, response http.ResponseWriter, statusCode int) {
+	errorStructure := ErrorResponse{"error", err.Error()}
+
+	json, err := json.Marshal(errorStructure)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(statusCode)
+	_, err = response.Write(json)
+
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type ErrorResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
