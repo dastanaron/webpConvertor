c2993ec443d5eadb101107d1e588e5b327dc8340
dastanaron <flow199@yandex.ru>
2021-06-29 22:32:56 +0300

    Separated convert and build parameters methods

--- src/main.go
+++ src/main.go
@@ -26,60 +26,28 @@ func main() {
 }
 
 func handleRequest(response http.ResponseWriter, request *http.Request) {
-	downloadedImage, err := downloadImage(request)
+	cwebp := convertor.NewCWebP()
+	cwebp.SetBinPath(appConfig.WebpLibPath)
+
+	err, resizeParameters, quality := buildConvertParameters(request)
+
 	if err != nil {
 		helpers.BuildErrorResponse(err, response, 422)
 		return
 	}
-	convert(response, request, downloadedImage)
+
+	cwebp.SetQuality(*quality)
+
+	downloadedImage, err := downloadImage(request)
+
+	if err != nil {
+		helpers.BuildErrorResponse(err, response, 422)
+		return
+	}
+	convert(response, *resizeParameters, downloadedImage, *cwebp)
 }
 
-func convert(response http.ResponseWriter, request *http.Request, imageBytes *bytes.Buffer) {
-	params := request.URL.Query()
-
-	cwebp := convertor.NewCWebP()
-	cwebp.SetBinPath(appConfig.WebpLibPath)
-
-	qualityParam, ok := params["q"]
-
-	var err error
-
-	if ok {
-		quality, err := strconv.Atoi(qualityParam[0])
-
-		if err == nil {
-			cwebp.SetQuality(quality)
-		}
-	}
-
-	resizeParameters := convertor.ResizeParameters{}
-	resizeParamWidth, ok := params["w"]
-
-	if ok {
-		resizeParameters.Width, err = strconv.Atoi(resizeParamWidth[0])
-
-		if err != nil {
-			helpers.BuildErrorResponse(errors.New("[w] parameter is not string"), response, 422)
-			return
-		}
-	}
-
-	resizeParamHeight, ok := params["h"]
-
-	if ok {
-		resizeParameters.Height, err = strconv.Atoi(resizeParamHeight[0])
-
-		if err != nil {
-			helpers.BuildErrorResponse(errors.New("[h] parameter is not string"), response, 422)
-			return
-		}
-	}
-
-	resizeParamType, ok := params["type"]
-
-	if ok {
-		resizeParameters.Type = resizeParamType[0]
-	}
+func convert(response http.ResponseWriter, resizeParameters convertor.ResizeParameters, imageBytes *bytes.Buffer, cwebp convertor.WebP) {
 
 	//computedResizeParameters, cropParameters, err := computeFitCropping(resizeParameters, image)
 
@@ -100,11 +68,6 @@ func convert(response http.ResponseWriter, request *http.Request, imageBytes *by
 
 	if resizeParameters.Type == "fit" {
 		dstImageResized = imaging.Fit(dstImage, resizeParameters.Width, resizeParameters.Height, imaging.Lanczos)
-	}
-
-	if (resizeParameters.Type != "") && (resizeParameters.Height == 0 || resizeParameters.Width == 0) {
-		helpers.BuildErrorResponse(errors.New("You need to specify the height and width if the type is specified"), response, 409)
-		return
 	}
 
 	if resizeParameters.Type == "" {
@@ -128,9 +91,56 @@ func convert(response http.ResponseWriter, request *http.Request, imageBytes *by
 	}
 }
 
-/*func buildConvertParameters(response http.ResponseWriter, request *http.Request) {
+func buildConvertParameters(request *http.Request) (error, *convertor.ResizeParameters, *int) {
+	params := request.URL.Query()
 
-}*/
+	qualityParam, ok := params["q"]
+
+	var err error
+
+	quality := 80
+
+	if ok {
+		formattedQuality, err := strconv.Atoi(qualityParam[0])
+
+		if err == nil {
+			quality = formattedQuality
+		}
+	}
+
+	resizeParameters := convertor.ResizeParameters{}
+	resizeParamWidth, ok := params["w"]
+
+	if ok {
+		resizeParameters.Width, err = strconv.Atoi(resizeParamWidth[0])
+
+		if err != nil {
+			return errors.New("[w] parameter is not string"), nil, nil
+		}
+	}
+
+	resizeParamHeight, ok := params["h"]
+
+	if ok {
+		resizeParameters.Height, err = strconv.Atoi(resizeParamHeight[0])
+
+		if err != nil {
+			return errors.New("[h] parameter is not string"), nil, nil
+		}
+	}
+
+	resizeParamType, ok := params["type"]
+
+	if ok {
+		resizeParameters.Type = resizeParamType[0]
+	}
+
+	if (resizeParameters.Type != "") && (resizeParameters.Height == 0 || resizeParameters.Width == 0) {
+		return errors.New("You need to specify the height and width if the type is specified"), nil, nil
+	}
+
+	return nil, &resizeParameters, &quality
+}
 
 func downloadImage(request *http.Request) (*bytes.Buffer, error) {
 	params := request.URL.Query()
